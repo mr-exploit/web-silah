1bead93a727d2efc47141a47b6a5ba880a98573f
mr-exploit <raffiaj8@gmail.com>
2022-12-25 12:32:53 +0700

    fix bugs host value

--- src/features/authSlice.js
+++ src/features/authSlice.js
@@ -11,7 +11,7 @@ const initialState = {
 
 export const LoginUser = createAsyncThunk("user/loginUser", async(user, thunkAPI) =>{
     try {
-        const response = await axios.post('http://localhost:5000/login', {
+        const response = await axios.post('http://server.silah.website:5000/login', {
             email: user.email,
             password: user.password
         });
@@ -26,7 +26,7 @@ export const LoginUser = createAsyncThunk("user/loginUser", async(user, thunkAPI
 
 export const getMe = createAsyncThunk("user/getMe", async(_, thunkAPI) =>{
     try {
-        const response = await axios.get('http://localhost:5000/me');
+        const response = await axios.get('http://server.silah.website:5000/me');
         return response.data;
     } catch (error) {
         if(error.response){
@@ -37,7 +37,7 @@ export const getMe = createAsyncThunk("user/getMe", async(_, thunkAPI) =>{
 });
 
 export const LogOut = createAsyncThunk("user/LogOut", async() =>{
-    await axios.delete('http://localhost:5000/logout');
+    await axios.delete('http://server.silah.website:5000/logout');
 });
 
 export const authSlice = createSlice({
